be9164e48eac802fd102d604bf9baaba8a95b0f8
Edu <barajas.eduardo@uabc.edu.mx>
2020-10-28 01:08:12 -0700

    test

---
+++ precache-manifest.3717ca96467d4b85d36ed8f35ae714c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc51ecdbb36119eb191b34170c1846e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feef14baea838482cd46",
+    "url": "/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "cb1035ce23293acd9aa5",
+    "url": "/static/css/main.2885d226.chunk.css"
+  },
+  {
+    "revision": "feef14baea838482cd46",
+    "url": "/static/js/2.b997e188.chunk.js"
+  },
+  {
+    "revision": "6ef9600e002249fa9b4dcb7e89c69c3b",
+    "url": "/static/js/2.b997e188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1035ce23293acd9aa5",
+    "url": "/static/js/main.f6d2d421.chunk.js"
+  },
+  {
+    "revision": "1a0bf5a665544d705e59",
+    "url": "/static/js/runtime-main.5a0c44ca.js"
+  },
+  {
+    "revision": "4b2e23214995eca1921174af44d39295",
+    "url": "/static/media/delivery_man.4b2e2321.png"
+  }
+]);
